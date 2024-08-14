22da517cecb8b07f587b1ba8c81b00f14b1d84ef
RedSunInTheSky <benjamin.perezr@usm.cl>
2024-08-13 23:11:59 -0400

    regex listos (ojalá)

--- Interprete.py
+++ Interprete.py
@@ -1,3 +1,11 @@
 import re
 file = open('codigo.txt','r')
-entero = r"^([1-9][0-9]*|0)"
+
+#EBNF
+define = r"^\s*DEFINE\s+\$_[A-Z][A-Za-z]*\s*$"
+asignacion = r"^\s*DP\s+\$_[A-Z][A-Za-z]*\s+ASIG\s+(True|False|\d+|#[^#]*#)\s*$"
+suma = r"^\s*DP\s+\$_[A-Z][A-Za-z]*\s+\+\s+(\$_[A-Z][A-Za-z]*|\d+|#[^#]*#)\s+(\$_[A-Z][A-Za-z]*|\d+|#[^#]*#)\s*$"
+multiplicacion = r"^\s*DP\s+\$_[A-Z][A-Za-z]*\s+\*\s+(\$_[A-Z][A-Za-z]*|\d+)\s+(\$_[A-Z][A-Za-z]*|\d+)\s*$"
+leer_if = r"^\s*if\s*\(\s*\$_[A-Z][A-Za-z]*\s*\)\s*\{\s*$"
+leer_else = r"^\s*\}\s*else\s*\{\s*$"
+mostrar = r"^\s*MOSTRAR\(\s*\$_[A-Z][A-Za-z]*\s*\)\s*$"
