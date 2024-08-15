a6b28c7e3d9f0bb2500d90c2bbd294e607a63682
RedSunInTheSky <benjamin.perezr@usm.cl>
2024-08-15 18:59:03 -0400

    por fin los errores listos XD, además de la declaración  de variables ojalá

--- Interprete.py
+++ Interprete.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 #EBNF
 define = r"^\s*DEFINE\s+\$_[A-Z][A-Za-z]*\s*$"
@@ -13,35 +14,37 @@ concatenacion_texto = r"^\s*DP\s+\$_[A-Z][A-Za-z]*\s+\+\s+(\$_[A-Z][A-Za-z]*|#[^
 archivo_output=open("output.txt", "w")
 
 variables = {}
+
 def verificador_linea(linea, numero_linea):
     
     linea = linea.strip()
+    error_encontrado = 0
 
     if re.match(define, linea):
         var_name = re.findall(r"\$_[A-Z][A-Za-z]*", linea)[0]
         if var_name in variables:
-            archivo_output.write(f"Línea {numero_linea}: Variable Ya Definida: La variable '{var_name}' ya se encuentra definida.")
-        variables[var_name] = None
-        return None
-
+            print("Línea", numero_linea, ": Variable Ya Definida: La variable", var_name, "ya se encuentra definida.")
+            error_encontrado = 1
+        else:
+            variables[var_name] = None
     elif re.match(asignacion, linea):
         parts = linea.split()
         var_name = parts[1]
         value = parts[3]
 
         if var_name not in variables:
-            archivo_output.write(f"Línea {numero_linea}: Variable No Definida: La variable '{var_name}' no ha sido definida o no se le ha asignado valor.")
-        if value.startswith("#") and value.endswith("#"):
-            variables[var_name] = value[1:-1] 
-        elif value.isdigit():
-            variables[var_name] = int(value)
-        elif value in ["True", "False"]:
-            variables[var_name] = value == "True"
+            print("Línea", numero_linea, ": Variable No Definida: La variable", var_name, "no ha sido definida o no se le ha asignado valor.")
+            error_encontrado = 1
         else:
-            archivo_output.write(f"Línea {numero_linea}: Tipo Incompatible: El valor '{value}' es incompatible con la operación.")
-
-        return None
-
+            if value.startswith("#") and value.endswith("#"):
+                variables[var_name] = value[1:-1] 
+            elif value.isdigit():
+                variables[var_name] = int(value)
+            elif value in ["True", "False"]:
+                variables[var_name] = value == "True"
+            else:
+                print("Línea", numero_linea, ": Tipo Incompatible: El valor", value, "es incompatible con la operación.")
+                error_encontrado = 1
     elif re.match(suma, linea) or re.match(multiplicacion, linea):
         parts = linea.split()
         var_name = parts[1]
@@ -49,43 +52,60 @@ def verificador_linea(linea, numero_linea):
         operand2 = parts[4]
 
         if var_name not in variables:
-            archivo_output.write(f"Línea {numero_linea}: Variable No Definida: La variable '{var_name}' no ha sido definida o no se le ha asignado valor.")
-
-        operand1_value = variables.get(operand1) if operand1.startswith("$_") else int(operand1)
-        operand2_value = variables.get(operand2) if operand2.startswith("$_") else int(operand2)
-
-        if operand1_value is None or operand2_value is None:
-            archivo_output.write(f"Línea {numero_linea}: Variable No Definida: Alguno de los operandos no ha sido definido o no se le ha asignado valor.")
-
-        if isinstance(operand1_value, int) and isinstance(operand2_value, int):
-            if "+" in linea:
-                variables[var_name] = operand1_value + operand2_value
-            elif "*" in linea:
-                variables[var_name] = operand1_value * operand2_value
+            print("Línea", numero_linea, ": Variable No Definida: La variable", var_name, "no ha sido definida o no se le ha asignado valor.")
+            error_encontrado = 1
         else:
-            archivo_output.write(f"Línea {numero_linea}: Tipo Incompatible: La operación en la línea es incompatible con el tipo de dato.")
+            operand1_value = variables.get(operand1) if operand1.startswith("$_") else int(operand1)
+            operand2_value = variables.get(operand2) if operand2.startswith("$_") else int(operand2)
 
-        return None
-
+            if operand1_value is None or operand2_value is None:
+                print("Línea", numero_linea, ": Variable No Definida: Alguno de los operandos no ha sido definido o no se le ha asignado valor.")
+                error_encontrado = 1
+            elif isinstance(operand1_value, int) and isinstance(operand2_value, int):
+                if "+" in linea:
+                    variables[var_name] = operand1_value + operand2_value
+                elif "*" in linea:
+                    variables[var_name] = operand1_value * operand2_value
+            else:
+                print("Línea", numero_linea, ": Tipo Incompatible: La operación en la línea es incompatible con el tipo de dato.")
+                error_encontrado = 1
     elif re.match(leer_if, linea):
         var_name = re.findall(r"\$_[A-Z][A-Za-z]*", linea)[0]
         if var_name not in variables or variables[var_name] is None:
-            archivo_output.write(f"Línea {numero_linea}: Variable No Definida: La variable '{var_name}' no ha sido definida o no se le ha asignado valor.")
-        if not isinstance(variables[var_name], bool):
-            archivo_output.write(f"Línea {numero_linea}: Tipo Incompatible: La variable '{var_name}' no es booleana.")
-        return None
-
+            print("Línea", numero_linea, ": Variable No Definida: La variable", var_name, "no ha sido definida o no se le ha asignado valor.")
+            error_encontrado = 1
+        elif not isinstance(variables[var_name], bool):
+            print("Línea", numero_linea, ": Tipo Incompatible: La variable", var_name, "no es booleana.")
+            error_encontrado = 1
     elif re.match(mostrar, linea):
         var_name = re.findall(r"\$_[A-Z][A-Za-z]*", linea)[0]
         if var_name not in variables:
-            archivo_output.write(f"Línea {numero_linea}: Variable No Definida: La variable '{var_name}' no ha sido definida.")
-        return None
+            print("Línea", numero_linea, ": Variable No Definida: La variable", var_name, "no ha sido definida.")
+            error_encontrado = 1
+    else:
+        print("Línea", numero_linea, ": Mal Sintaxis: La línea no está bien escrita.")
+        error_encontrado = 1
 
-    archivo_output.write(f"Línea {numero_linea}: Mal Sintaxis: La línea no está bien escrita.")
+    return error_encontrado
 
-
+def definir_variable(linea):
+    linea = linea.strip()
+    matches = re.findall(r"\$_[A-Z][A-Za-z]*", linea)
+    nombre_var = matches[0]
+    variables[0] = nombre_var
+    variables[nombre_var] = None
+    
 with open("codigo.txt") as file_object:
     lista=file_object.readlines()
 i = 1
+hayerror = 0
 for linea in lista:
-    verificador_linea(linea, i)
+    hayerror = verificador_linea(linea, i)
+    if hayerror == 0:
+        i = 1#mentira
+    elif hayerror == 1:
+        archivo_output.close()
+        file_object.close()
+        sys.exit()
+    
+    
